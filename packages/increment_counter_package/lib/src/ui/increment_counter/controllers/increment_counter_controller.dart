f1cd918fa82ab347a38ef0885d23a18c4c019565
samuelematias <hi@samuelematias.com>
2020-08-28 13:03:10 -0300

    feat(controller): add IncrementCounterController

--- packages/increment_counter_package/lib/src/ui/increment_counter/controllers/increment_counter_controller.dart
+++ packages/increment_counter_package/lib/src/ui/increment_counter/controllers/increment_counter_controller.dart
@@ -1,1 +1,12 @@
-class IncrementCounterController {}
+import 'package:mobx/mobx.dart';
+
+class IncrementCounterController {
+  IncrementCounterController() {
+    increment = Action(() => _increment());
+  }
+
+  Observable<int> conuter = Observable(0);
+  Action increment;
+
+  int _increment() => conuter.value = conuter.value + 1;
+}
