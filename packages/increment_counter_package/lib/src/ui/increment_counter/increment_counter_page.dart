cfc464fc438a641ea03c4020844f32f94303e467
samuelematias <hi@samuelematias.com>
2020-08-28 12:07:51 -0300

    feat(package): create page/test on package

---
+++ packages/increment_counter_package/lib/src/ui/increment_counter/increment_counter_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class IncrementCounterPage extends StatelessWidget {
+  const IncrementCounterPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    const int _counter = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('title', title));
+  }
+}
--- packages/increment_counter_package/test/increment_counter_package_test.dart
+++ packages/increment_counter_package/test/increment_counter_package_test.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:increment_counter_package/increment_counter_package.dart';
+import 'package:increment_counter_package/src/ui/increment_counter/increment_counter_page.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  // Create a MaterialApp to test.
+  Widget _makeTestable(Widget widget) => MaterialApp(home: widget);
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Create the widget to test.
+    const widget = IncrementCounterPage(title: 'IncrementCounterPage Test');
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(_makeTestable(widget));
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
   });
 }
