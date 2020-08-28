ccd56ec7ce5e8451f1525d810b5041b8cc4e15d4
samuelematias <hi@samuelematias.com>
2020-08-28 13:15:20 -0300

    feat(controller): add autorun as example

--- packages/increment_counter_package/lib/src/ui/increment_counter/controllers/increment_counter_controller.dart
+++ packages/increment_counter_package/lib/src/ui/increment_counter/controllers/increment_counter_controller.dart
@@ -3,6 +3,10 @@ import 'package:mobx/mobx.dart';
 class IncrementCounterController {
   IncrementCounterController() {
     increment = Action(() => _increment());
+
+    autorun((_) {
+      print(conuter.value);
+    });
   }
 
   Observable<int> conuter = Observable(0);
