03413cbaab5250ba8ea78f427234798fea69c78f
samuelematias <hi@samuelematias.com>
2020-08-28 10:04:24 -0300

    feat(scripts): create scripts to help on CI/CD

---
+++ flutter_packages.sh
@@ -0,0 +1,16 @@
+# This script is used in CI/CD to pub get all packages
+BLUE="\e[36;1m%s\e[0m\n"
+GREEN="\e[32;1m%s\e[0m\n"
+YELLOW="\e[33;1m%s\e[0m\n"
+printf "${BLUE}" 'Starting get in all packages'
+for dir in packages/*
+do
+  cd $dir
+  printf "${GREEN}" "Get in: $dir"
+  flutter packages get $dir
+  cd -
+done
+# pub get on root
+# printf "${GREEN}" "Get in: root"
+# flutter packages get
+printf "${BLUE}" 'Finished get in all packages'
