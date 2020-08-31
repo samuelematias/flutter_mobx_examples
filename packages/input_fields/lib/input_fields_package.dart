ef0af4bde4bc3f0994aa002326c81ec6f0b48cbd
samuelematias <hi@samuelematias.com>
2020-08-31 17:47:18 -0300

    feat(pages): create input_fields page

--- packages/input_fields/lib/input_fields_package.dart
+++ packages/input_fields/lib/input_fields_package.dart
@@ -1,1 +1,3 @@
 library input_fields_package;
+
+export 'src/ui/input_fields/pages/input_fields_page.dart';
--- packages/input_fields/lib/src/ui/input_fields/pages/input_fields_page.dart
+++ packages/input_fields/lib/src/ui/input_fields/pages/input_fields_page.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 
 class InputFieldsPage extends StatelessWidget {
+  const InputFieldsPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input Fields'),
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildTexfield(labelText: 'Nome'),
+              _buildTexfield(labelText: 'Sobrenome'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget _buildTexfield({@required String labelText}) => Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: labelText),
+          ),
+          const SizedBox(height: 20)
+        ],
+      );
 }
