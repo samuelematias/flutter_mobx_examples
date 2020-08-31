936d6e52d561ab6c48d57d49046e0a13e212f3d0
samuelematias <hi@samuelematias.com>
2020-08-31 18:08:13 -0300

    feat(controller): add last/name on controller

--- packages/input_fields/lib/src/ui/input_fields/controllers/input_fields_controller.dart
+++ packages/input_fields/lib/src/ui/input_fields/controllers/input_fields_controller.dart
@@ -1,1 +1,20 @@
-class InputFieldsFontroller {}
+import 'package:flutter/foundation.dart';
+import 'package:mobx/mobx.dart';
+part 'input_fields_controller.g.dart';
+
+class InputFieldsController = InputFieldsControllerBase
+    with _$InputFieldsController;
+
+abstract class InputFieldsControllerBase with Store {
+  @observable
+  String name = '';
+
+  @observable
+  String lastName = '';
+
+  @action
+  String changeName({@required String newName}) => name = newName;
+
+  @action
+  String changeLastName({@required String newName}) => lastName = newName;
+}
