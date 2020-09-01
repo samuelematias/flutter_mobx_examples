d542d53b01885630806eade59bfaf720da34d053
samuelematias <hi@samuelematias.com>
2020-09-01 18:48:44 -0300

    feat(dispose): add dispose on FormPage

--- packages/form/lib/src/ui/pages/form/form_page.dart
+++ packages/form/lib/src/ui/pages/form/form_page.dart
@@ -3,17 +3,31 @@ import 'package:flutter/material.dart';
 import '../form/controllers/form_controller.dart';
 import '../form/widgets/body.dart';
 
-class FormPage extends StatelessWidget {
+class FormPage extends StatefulWidget {
   const FormPage({Key key}) : super(key: key);
+
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  final _controller = GetIt.I.get<FormController>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.client.changeName(newName: '');
+    _controller.client.changeEmail(newEmail: '');
+    _controller.client.changeCpf(newCpf: '');
+  }
+
   @override
   Widget build(BuildContext context) {
-    final controller = GetIt.I.get<FormController>();
-
     return Scaffold(
       appBar: AppBar(
         title: Observer(
           builder: (_) =>
-              Text(controller.isValid ? 'Valid form' : 'Invalid form'),
+              Text(_controller.isValid ? 'Valid form' : 'Invalid form'),
         ),
       ),
       body: const BodyWidget(),
