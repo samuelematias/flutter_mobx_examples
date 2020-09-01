0e00cf9035904acc93fe2ddf6e3ec79f88e535b0
samuelematias <hi@samuelematias.com>
2020-09-01 17:01:30 -0300

    feat(plugin): add provider

--- packages/common/lib/common_package.dart
+++ packages/common/lib/common_package.dart
@@ -4,3 +4,4 @@ export 'package:mobx/mobx.dart';
 export 'package:flutter_mobx/flutter_mobx.dart' hide version;
 export 'package:custom_buttons/custom_buttons.dart';
 export 'package:extensions/extensions.dart';
+export 'package:provider/provider.dart' hide Dispose;
