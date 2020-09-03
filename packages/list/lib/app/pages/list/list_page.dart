fbf898e6c761c28cf43544ef69ba5a9f53af1ced
samuelematias <hi@samuelematias.com>
2020-09-03 10:28:38 -0300

    feat(page): create last_page

--- packages/list/lib/app/pages/list/list_page.dart
+++ packages/list/lib/app/pages/list/list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'components/item_widget.dart';
+
 class ListPage extends StatefulWidget {
   const ListPage({Key key}) : super(key: key);
   @override
@@ -9,6 +11,41 @@ class ListPage extends StatefulWidget {
 class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const TextField(
+          decoration: InputDecoration(hintText: 'Search...'),
+        ),
+      ),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _buildDialog(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
+
+  Widget _buildBody() => ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, index) => const Itemwidget(),
+      );
+
+  Future<void> _buildDialog() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Add items'),
+          content: TextField(
+            decoration: const InputDecoration(
+                labelText: 'New item', border: OutlineInputBorder()),
+            onChanged: (value) {},
+          ),
+          actions: <Widget>[
+            FlatButton(onPressed: () {}, child: const Text('Save')),
+            FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel')),
+          ],
+        ),
+      );
 }
